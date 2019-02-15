8592595533d0ca4026eab8a588cc14ff7d4382ee
cherylac <38063622+cherylac@users.noreply.github.com>
2019-02-14 19:58:15 -0800

    first push

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app" class="container">
+  <h1>Hello App Vue</h1>
+  <router-view/>
+     
+  </div>
+</template>
+<script>
+// import Firebase from 'firebase'
+// import toastr from 'toastr'
+// let config = {
+  //   apiKey: "...",
+  //   authDomain: "...",
+  //   databaseURL: "...",
+  //   storageBucket: "...",
+  //   messagingSenderId: "..."
+  // };
+  
+// let app = Firebase.initializeApp(config)
+// let db = app.database()
+// let booksRef = db.ref('books')
+
+export default {
+  name: 'app',
+  // firebase: {
+  //   books: booksRef
+  // },
+  
+  // data () {
+  //   return {
+  //     newBook: {
+  //         title: '',
+  //         author: '',
+  //         url: 'http://'
+  //     }
+  //   }
+  // }
+  
+   
+  
+ 
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+// import VueFire from 'vuefire'
+import App from './App'
+import router from './router'
+
+/* eslint-disable no-new */
+// Vue.use(VueFire)
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  router,
+  components: { App }
+})
+
+
+
+
