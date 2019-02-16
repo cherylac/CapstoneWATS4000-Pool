9945b2cca7d61f49c7f491945d16b5505c7bcfe4
cherylac <38063622+cherylac@users.noreply.github.com>
2019-02-16 15:24:08 -0800

    changed data to baby pool

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,62 @@
 <template>
   <div id="app" class="container">
     <div class="page-header">
-  <h1>Hello Books!</h1>
+  <h1>Baby Pool!</h1>
   </div>
   <div class="panel panel-default">
     <div class="panel-heading">
-      <h3 class="panel-title">Add New Books</h3>
+      <h3 class="panel-title">Contest A</h3>
     </div>
     <div class="panel-body">
-      <form id="form" class="form-inline" v-on:submit.prevent="addBook">
+      <form id="form" class="form-inline" v-on:submit.prevent="addEntry">
         <div class="form-group">
-          <label for="bookAuthor">Author:</label>
-          <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author">
+          <label for="entryContestant">Your name:</label>
+          <input type="text" id="entryContestant" class="form-control" v-model="newEntry.contestant">
         </div>
         <div class="form-group">
-          <label for="bookTitle">Title:</label>
-          <input type="text" id="bookTitle" class="form-control" v-model="newBook.title">
+          <label for="entryEmail">Email:</label>
+          <input type="text" id="entryTitle" class="form-control" v-model="newEntry.email">
         </div>
         <div class="form-group">
-          <label for="bookUrl">Url:</label>
-          <input type="text" id="bookUrl" class="form-control" v-model="newBook.url">
+          <label for="bookUrl">Guess Date:</label>
+          <input type="text" id="entryDate" class="form-control" v-model="newEntry.date">
         </div>
-        <input type="submit" class="btn btn-primary" value="Add Book">
+        <div class="form-group">
+          <label for="entryWeight">Guess Weight:</label>
+          <input type="text" id="entryWeight" class="form-control" v-model="newEntry.weight">
+        </div>
+        <div class="form-group">
+          <label for="entryBabyname">Guess Name:</label>
+          <input type="text" id="entryBabyname" class="form-control" v-model="newEntry.babyname">
+        </div>
+        <input type="submit" class="btn btn-primary" value="Add Entry">
       </form>
     </div>
   </div>
   <div class="panel panel-default">
     <div class="panel-heading">
-      <h3 class="panel-title">Book List</h3>
+      <h3 class="panel-title">Entries</h3>
       </div>
   <div class="panel-body">
     <table class="table table-striped">
       <thead>
         <tr>
-          <th>Author</th>
-          <th>Title</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Date</th>
+          <th>Weight</th>
+          <th>Baby's Name</th>
           <th></th>
          </tr>
         </thead>
         <tbody>
-          <tr v-for="book in books">
-            <td><a v-bind:href="book.url">{{book.author}}</a></td>
-            <td>{{book.title}}</td>
-            <td><span class="glyphicon glyphicon-trash" aria-hidden="true" v-on:click="removeBook(book)"></span></td>
+          <tr v-for="entry in entries">
+            <td>{{entry.contestant}}</td>
+            <td>{{entry.email}}</td>
+            <td>{{entry.date}}</td>
+            <td>{{entry.weight}}</td>
+            <td>{{entry.babyname}}</td>
+            <td><span class="glyphicon glyphicon-trash" aria-hidden="true" v-on:click="removeEntry(entry)"></span></td>
           </tr>
         </tbody>
     </table>
@@ -54,44 +68,48 @@
 import Firebase from 'firebase'
 import toastr from 'toastr'
  let config = {
-    apiKey: "AIzaSyBr9Ig1eWoBWfjvgvlJkqO3SFYKEfpQhrE",
-    authDomain: "vuejs-firebase-01-89ffe.firebaseapp.com",
-    databaseURL: "https://vuejs-firebase-01-89ffe.firebaseio.com",
-    projectId: "vuejs-firebase-01-89ffe",
-    storageBucket: "vuejs-firebase-01-89ffe.appspot.com",
-    messagingSenderId: "288122196180"
+    apiKey: "AIzaSyCQphEKrAx7M6kCA0wqlj2QICzcrycLpeA",
+    authDomain: "birthdate-pool.firebaseapp.com",
+    databaseURL: "https://birthdate-pool.firebaseio.com",
+    projectId: "birthdate-pool",
+    storageBucket: "birthdate-pool.appspot.com",
+    messagingSenderId: "838139933356"
   };
   
 let app = Firebase.initializeApp(config)
 let db = app.database()
-let booksRef = db.ref('books')
+let entriesRef = db.ref('entries')
 
 export default {
   name: 'app',
   firebase: {
-    books: booksRef
+    entries: entriesRef
   },
   
   data () {
     return {
-      newBook: {
-          author: '',
-          title: '',
-          url: 'http://'
-      }
+      newEntry: {
+          contestant: '',
+          email: '',
+          date: '',
+          weight:'',
+          babyname:''
     }
+  }
   },
   
 methods: {
-  addBook: function () {
-    booksRef.push(this.newBook);
-    this.newBook.author = '';
-    this.newBook.title = '';
-    this.newBook.url = 'http://';
+  addEntry: function () {
+    entriesRef.push(this.newEntry);
+    this.newEntry.contestant = '';
+    this.newEntry.email = '';
+    this.newEntry.date = '';
+    this.newEntry.weight = '';
+    this.newEntry.babyname = '';
   },
-  removeBook: function (book) {
-    booksRef.child(book['.key']).remove()
-    toastr.success('Book removed successfully')
+  removeEntry: function (entry) {
+    entriesRef.child(entry['.key']).remove()
+    toastr.success('Entry removed successfully')
   }
   }
 }
