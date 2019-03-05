2cc8f276c36a0c7883af57fca0d588dda6a424a7
cherylac <38063622+cherylac@users.noreply.github.com>
2019-03-05 01:33:47 -0800

    added some styling

--- src/App.vue
+++ src/App.vue
@@ -121,10 +121,22 @@ methods: {
 </script>
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Handlee', cursive;
+  /* 'Avenir', Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 20px;
+}
+
+.jumbotron {
+  text-align: center;
+  font-family: 'Handlee', cursive;
+  background-color: 	#f8ab99;
+  color:#f7fffc; 
+}
+
+.btn-primary {
+  background-color:#83daba; 
 }
 </style>
